9034df3684d5e53774ae3f1c66acab72ffe06280
Pedro Soares <pedropaulos.1998@gmail.com>
2020-03-25 01:37:32 -0300

    exclusion route implemented

--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,18 @@
 const express = require('express');
 
 const OngController = require('./controllers/OngController');
-const IncidentController = require('./controllers/IncidentController')
+const IncidentController = require('./controllers/IncidentController');
+const ProfileControlles = require('./controllers/ProfileController');
 
 const routes = express.Router();
 
 routes.get('/ongs', OngController.index);
 routes.post('/ongs', OngController.create);
 
+routes.get('/profile', ProfileControlles.index);
+
 routes.get('/incidents', IncidentController.index);
 routes.post('/incidents', IncidentController.create);
+routes.delete('/incidents/:id', IncidentController.delete);
 
 module.exports = routes;
