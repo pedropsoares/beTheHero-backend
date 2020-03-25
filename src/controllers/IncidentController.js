e10414d94044ce16cc59ebf42d0ee23a06e953d6
Pedro Soares <pedropaulos.1998@gmail.com>
2020-03-25 01:00:12 -0300

    implemented controllers

---
+++ src/controllers/IncidentController.js
@@ -0,0 +1,23 @@
+const connection = require('../database/connection');
+
+module.exports = {
+  async index(req, res)  {
+    const incident = await connection('incidents').select('*');
+  
+    return res.json(incident);
+  },
+
+  async create(req, res) {
+    const { title, description, value } = req.body;
+    const ong_id = req.headers.authorization;
+
+    const [id] = await connection('incidents').insert({
+      title,
+      description,
+      value,
+      ong_id
+    });
+
+    return res.json({ id });
+  }
+}
